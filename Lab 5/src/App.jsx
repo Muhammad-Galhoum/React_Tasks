6e65a6961aca7af487edf550b29ba73096c6084b
Muhammad Galhoum <56798376+Muhammad-Galhoum@users.noreply.github.com>
2021-11-02 16:16:37 +0200

    Lab 5 (Last lab)

---
+++ Lab 5/src/App.jsx
@@ -0,0 +1,37 @@
+import { Route, Redirect, Switch } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { Students, Posts, Users, UserDetails, ContactUs, Login } from "./Pages";
+
+export const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <ProtectedRoute path="/students">
+            <Students />
+          </ProtectedRoute>
+          <ProtectedRoute path="/posts">
+            <Posts />
+          </ProtectedRoute>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+          <Route path="/users/:id">
+            <UserDetails />
+          </Route>
+          <ProtectedRoute path="/users">
+            <Users />
+          </ProtectedRoute>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Redirect to="/students" />
+          </Route>
+        </Switch>
+      </div>
+    </>
+  );
+};
